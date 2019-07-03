1a6045435d3fac3d518abac558c3a7cede54bdf4
Pavel <wol1414@gmail.com>
2019-07-03 19:12:43 +0300

    edit text

--- public/js/common.js
+++ public/js/common.js
@@ -615,3 +615,14 @@ JSCCommon = {
 JSCCommon.LazyFunction();
 /***/
 
+
+Share = {
+	me : function(el){
+			Share.popup(el.href);
+			return false;
+	},
+
+	popup: function(url) {
+			window.open(url,'','toolbar=0,status=0,width=626,height=436');
+	}
+};
